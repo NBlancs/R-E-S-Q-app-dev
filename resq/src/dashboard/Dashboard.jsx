e495400ea98d85e551913019d33192016a1adc99
Tesoi25 <soria.glenn1995@gmail.com>
2026-02-11 12:10:04 +0800

    change Gas

--- resq/src/dashboard/Dashboard.jsx
+++ resq/src/dashboard/Dashboard.jsx
@@ -37,7 +37,7 @@ const Dashboard = ({ onLogout }) => {
     },
     {
       id: '#INC-002',
-      type: 'Smoke',
+      type: 'Gas',
       location: 'Zone C - Lobby',
       method: 'Camera AI',
       time: '08:30 AM',
