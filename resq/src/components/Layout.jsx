ed2eafa8355120c9687cbf844a44848f4e385268
NBlancs <bnoel.omar@gmail.com>
2026-02-22 00:03:05 +0800

    fixed footer bug

--- resq/src/components/Layout.jsx
+++ resq/src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
+import Footer from './Footer';
 
 const Layout = ({ onLogout }) => {
   return (
@@ -8,6 +9,7 @@ const Layout = ({ onLogout }) => {
       <main className="dashboard-content">
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 };
